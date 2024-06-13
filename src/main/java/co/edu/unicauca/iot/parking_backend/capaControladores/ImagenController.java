10c2d3d5bcf09809edcfe43223f16c829e42311d
Esteban Daza <daesteban@unicauca.edu.co>
2024-06-13 08:35:06 -0500

    persimos

--- src/main/java/co/edu/unicauca/iot/parking_backend/capaControladores/ImagenController.java
+++ src/main/java/co/edu/unicauca/iot/parking_backend/capaControladores/ImagenController.java
@@ -10,9 +10,7 @@ import java.io.IOException;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
@@ -41,7 +39,7 @@ public class ImagenController {
         }
     }
 
-    @PostMapping("/upload")
+    @PostMapping("/cargar")
     public ResponseEntity<String> uploadImagen(@RequestPart("imagen") MultipartFile imagen) {
         try {
             Imagen imagenGuardada = imagenService.guardarImagen(imagen);
